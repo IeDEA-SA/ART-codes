998373855f60c4151cd4f00cb2f7b55dd27b2301
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-11-14 12:15:28 +0100

    link between art_id & drug codes
    can be used to identify lack of overlap

---
+++ R/link.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(fst)
+
+druglist = readr::read_csv("https://raw.githubusercontent.com/IeDEA-SA/ART-codes/main/data-raw/IeDEA_druglist.csv") %>% 
+  select(art_id, drug, arv_class) %>% 
+  mutate(drug = str_trim(drug))
+
+# note there are different art_id codes included in DTG family
+tblART = read_fst("../IeDEA_core/data/2022_07_08 IeDEA SA DES/tblART.fst") %>% 
+  as_tibble() %>% 
+  mutate(art_id = str_trim(art_id))
+
+# in database without link to codes
+missings = anti_join(tblART, druglist) 
+
+table(missings$art_id)
+
+druglist %>% 
+  filter(art_id == "J05AJ01")
+
+druglist %>% 
+  filter(art_id == "J05AJ01")
+
+# in lookup table without existing in database
+missings = anti_join(druglist, tblART) 
+
+missings$art_id
